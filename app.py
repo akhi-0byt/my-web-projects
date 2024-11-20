f1ac5b4a60797368a9ce4c55781d85bc4318a3b6
akhil rajput <sobindchauhan31@gmail.com>
2024-11-20 14:50:31 +0530

    Update app.py
    i add simple comments to understands the my codes . anybody can read my codes and teach other easly

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-print("to coding journay kaisi chal rhi hai ")
-print(56+65)
+# This line prints a message on the screen
+print("to coding journey kaisi chal rhi hai ")
+
+# This line adds 56 and 65 and then prints the result
+print(56 + 65)
+
+# This part checks if the Flask module can be imported
 try:
-  import flask
-  print("Flask import ho gaya hai!")
+    import flask  # Try to import the Flask module
+    print("Flask import ho gaya hai!")  # If it works, print this message
 except ImportError:
-  print("Flask import nahi hua hai.")
-from flask import Flask, app
+    print("Flask import nahi hua hai.")  # If it doesn't work, print this message
+
+# Import Flask from the flask library
+from flask import Flask
+
+# Create an instance of the Flask application
 app = Flask(__name__)
 
+# Define what happens when the user visits the homepage ('/')
 @app.route('/')
 def hello_world():
-    return 'i am trying to macking a web page '
-if __name__== "__main__":
-   app.run(host='0.0.0.0',debug=True)
+    return 'i am trying to making a web page '  # Send this message to the user
+
+# Make sure the server runs only if this script is run directly
+if __name__ == "__main__":
+    # Start the Flask web server
+    app.run(host='0.0.0.0', debug=True)
